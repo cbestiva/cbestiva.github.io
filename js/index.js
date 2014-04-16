9e58b7c9dd0239de93ad9a57276bd7f0fda7d8bf
cbestiva <cbestiva@gmail.com>
2014-04-16 12:41:49 -0700

    v1 of web portfolio

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function hover(element) {
+  element.setAttribute('src', 'img/circle_photo2_with_border.png');
+}
+
+function unhover(element) {
+  element.setAttribute('src', 'img/circle_photo_with_border.png');
+}
