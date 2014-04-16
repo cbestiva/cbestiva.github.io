e7520465cacf28e0de9c2e74b89773a11ab2aca6
cbestiva <cbestiva@gmail.com>
2014-04-16 12:51:34 -0700

    corrected js file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-var initialize = function() {
   function hover(element) {
     element.setAttribute('src', 'img/circle_photo2_with_border.png');
   }
@@ -6,6 +5,3 @@ var initialize = function() {
   function unhover(element) {
     element.setAttribute('src', 'img/circle_photo_with_border.png');
   }
-};
-
-$(document).on('ready page:load', initialize);
