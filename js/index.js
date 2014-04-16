cd955840aed9b6673af36dc7d0edab671716ca65
cbestiva <cbestiva@gmail.com>
2014-04-16 12:47:40 -0700

    fixed document load

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-function hover(element) {
-  element.setAttribute('src', 'img/circle_photo2_with_border.png');
-}
+var initialize = function() {
+  function hover(element) {
+    element.setAttribute('src', 'img/circle_photo2_with_border.png');
+  }
 
-function unhover(element) {
-  element.setAttribute('src', 'img/circle_photo_with_border.png');
-}
+  function unhover(element) {
+    element.setAttribute('src', 'img/circle_photo_with_border.png');
+  }
+};
+
+$(document).on('ready page:load', initialize);
